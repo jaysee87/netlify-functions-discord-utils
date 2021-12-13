f4500d4eb7da3baa5a4a30548062e2e97ef8127a
jaysee87 <jayseespeedruns@gmail.com>
2021-12-12 20:59:25 -0600

    Update callback.js

--- src/functions/callback.js
+++ src/functions/callback.js
@@ -1,29 +1,23 @@
 const axios = require('axios')
+const url = require('url')
 
 exports.handler = async function(event, context) {
   const baseUrl = `https://discord.com/api`
 
-  const data = {
+  const data = new url.URLSearchParams({
     grant_type: 'authorization_code',
     code: event.queryStringParameters.code,
     client_id: process.env.CLIENT_ID,
     client_secret: process.env.CLIENT_SECRET,
     scope: process.env.OAUTH_SCOPES,
     redirect_uri: process.env.REDIRECT_URI,
-  }
+  })
 
   const url = `${baseUrl}/oauth2/token`
 
   console.log('hello');
   try {
-    const response = await axios.post(url, {
-      grant_type: 'authorization_code',
-      code: event.queryStringParameters.code,
-      client_id: process.env.CLIENT_ID,
-      client_secret: process.env.CLIENT_SECRET,
-      scope: process.env.OAUTH_SCOPES,
-      redirect_uri: process.env.REDIRECT_URI,
-    },{ 
+    const response = await axios.post(url, data.toString(),{ 
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded'
         },
